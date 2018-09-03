ce93b0386217007029d85454a714f099cab17086
JerLai <jlai083@live.fr>
2018-09-04 00:41:54 +0200

    finalisation

---
+++ LancerApplication.java
@@ -0,0 +1,13 @@
+// CLASSE DONNEE AUX ETUDIANTS
+// NE PAS MODIFIER
+// Vous n'avez pas à comprendre ni à modifier cette classe
+
+import javafx.application.Application;
+
+class LancerApplication{
+	
+	public static void main(String[] args) {
+      Application.launch(Vue.class,args);
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,7 @@
+#Space Invader
+
+Pour lancer l'application dans un terminal (compiler) : 
+**javac *.java**
+
+puis (lancer) :
+**java LancerApplication**
